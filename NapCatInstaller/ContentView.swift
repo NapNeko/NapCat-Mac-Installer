72011c5283b008022e6f743a53eff0bb8a6c94b8
Hao Guan <10684225+hguandl@users.noreply.github.com>
2024-10-01 23:10:58 +0800

    Initial Commit

---
+++ NapCatInstaller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NapCatInstaller
+//
+//  Created by hguandl on 2024/10/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NapCatInstaller/NapCatInstallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NapCatInstallerApp.swift
+//  NapCatInstaller
+//
+//  Created by hguandl on 2024/10/1.
+//
+
+import SwiftUI
+
+@main
+struct NapCatInstallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
